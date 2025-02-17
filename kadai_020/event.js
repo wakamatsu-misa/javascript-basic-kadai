8f34ef6b88d06715a67731086407d4e8221a5978
wakamatsu-misa <misamisaoor@gmail.com>
2025-02-17 20:30:19 +0900

    jsを修正した

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,6 @@
 //btnとtextというidをHTMLから所得して定数に代入する
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
-//ボタンを押したら文字が「ボタンがクリックされました」へ変更する処理
 btn.addEventListener('click', () => {
-      btn.textContent='ボタンがクリックされました';
+      text.textContent='ボタンがクリックされました';
 });
